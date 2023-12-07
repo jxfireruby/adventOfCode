af4db9cc68c4a75ab3d7713c90e31ded8d455a07
Jason <13365557+jxfireruby@users.noreply.github.com>
2023-12-06 20:52:38 -0500

    Fix Day3 Part2 Code

--- 2023/adventOfCode2023Day3Part2.py
+++ 2023/adventOfCode2023Day3Part2.py
@@ -26,60 +26,38 @@ def day_3_part_2(file_name):
                         file_lines[i][right_end]
                     except:
                         right_end = match.end()
-                    # Match is faster with regex starting. Search if faster if search value is string literal
-                    digit = re.match(r'\d', file_lines[i][left_start])
 
-                    # find whole number after finding digit
-                    if digit:
-    #                    print(match.group(0))
-                        gear_number_in_line = re.finditer(r'\d+', file_lines[i][0:match.start()])
+                    # Find Number to left or right of gear
+                    gear_number_in_line = re.finditer(r'\d+', file_lines[i])
+                    for gear_number_candidate in gear_number_in_line:
+                        if gear_number_candidate.end() == match.start() or gear_number_candidate.start() == match.end():
+                            print("same line", int(gear_number_candidate.group(0)), end=" ")
+                            gears.append(int(gear_number_candidate.group(0)))
+
+                    # Find number in row above
+                    if i > 0:
+                        line_index = i - 1
+                        gear_number_in_line = re.finditer(r'\d+', file_lines[line_index])
                         for gear_number_candidate in gear_number_in_line:
-                            if gear_number_candidate.start() < right_end and gear_number_candidate.end() > left_start:
-                                print("left right", left_start, right_end)
-                                print("gear left, right", gear_number_candidate.start(), gear_number_candidate.end())
-                                print("gear", gear_number_candidate.group(0))
+                            if gear_number_candidate.end() >= match.start() and gear_number_candidate.start() <= match.end():
+                                print("above line", int(gear_number_candidate.group(0)), end=" ")
                                 gears.append(int(gear_number_candidate.group(0)))
                     
-                    # right_end is for the end of the range. Last digit is range - 1
-                    digit = re.match(r'\d', file_lines[i][right_end - 1])
-
-                    if digit:
-    #                    print(match.group(0))
-                        gear_number_in_line = re.finditer(r'\d+', file_lines[i][match.end():])
+                    # Find number in row below
+                    if i < len(file_lines) - 1:
+                        line_index = i + 1
+                        gear_number_in_line = re.finditer(r'\d+', file_lines[line_index])
                         for gear_number_candidate in gear_number_in_line:
-                            if gear_number_candidate.start() < right_end and gear_number_candidate.end() > left_start:
-                                print("left right", left_start, right_end)
-                                print("gear left, right", gear_number_candidate.start(), gear_number_candidate.end())
-                                print("gear", gear_number_candidate.group(0))
+                            if gear_number_candidate.end() >= match.start() and gear_number_candidate.start() <= match.end():
+                                print("below line", int(gear_number_candidate.group(0)), end=" ")
                                 gears.append(int(gear_number_candidate.group(0)))
-
-                    for number_index in range(left_start, right_end):
-                        if i > 0:
-                            line_index = i - 1
-                            digit = re.match(r'\d', file_lines[line_index][number_index])
-                            if digit:
-                                gear_number_in_line = re.finditer(r'\d+', file_lines[line_index])
-                                for gear_number_candidate in gear_number_in_line:
-                                    if gear_number_candidate.start() < right_end and gear_number_candidate.end() > left_start:
-                                        print("left right", left_start, right_end)
-                                        print("gear left, right", gear_number_candidate.start(), gear_number_candidate.end())
-                                        print("gear", gear_number_candidate.group(0))
-                                        gears.append(int(gear_number_candidate.group(0)))
-                        if i < len(file_lines) - 1:
-                            line_index = i + 1
-                            digit = re.match(r'\d', file_lines[line_index][number_index])
-                            if digit:
-                                gear_number_in_line = re.finditer(r'\d+', file_lines[line_index])
-                                for gear_number_candidate in gear_number_in_line:
-                                    if gear_number_candidate.start() < right_end and gear_number_candidate.end() > left_start:
-                                        print("left right", left_start, right_end)
-                                        print("gear left, right", gear_number_candidate.start(), gear_number_candidate.end())
-                                        print("gear", gear_number_candidate.group(0))
-                                        gears.append(int(gear_number_candidate.group(0)))
-                    # Only part that has to change if assumption is wrong
+                    print()
+                    for b in gears:
+                        print(b, end=" ")
+                    print()
                     if len(gears) >= 2:
-                        print(gears[0], gears[1])
                         sum = sum + (gears[0] * gears[1])
+                        print(sum)
         return sum
 
 if __name__ == '__main__':
